4d50de400b9e3514b5df816f12ab62f76ac80a9c
VivaanShin <xhatmtjfla23@gmail.com>
2020-08-23 01:46:30 +0900

    v_mysql first commit -yj

--- sensiblebox/routes/index.js
+++ sensiblebox/routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const mysql=require('mysql');
-
+//브런치 추가용
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Sensiblebox _ SmithGround' });
